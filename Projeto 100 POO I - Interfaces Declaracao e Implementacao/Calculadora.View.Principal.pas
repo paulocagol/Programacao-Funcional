7d0dbdfeb93845bc092c7b9241b321908b6bc70d
Paulo J. Cagol <paulojosecagol@gmail.com>
2021-11-09 23:33:02 -0300

    Projeto 100% POO VI - Refatoração

--- Projeto 100 POO I - Interfaces Declaracao e Implementacao/Calculadora.View.Principal.pas
+++ Projeto 100 POO I - Interfaces Declaracao e Implementacao/Calculadora.View.Principal.pas
@@ -4,7 +4,8 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Classe.Calculadora;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Classe.Calculadora,
+  Calculadora.Interfaces;
 
 type
   TForm1 = class(TForm)
--- Projeto 100 POO I - Interfaces Declaracao e Implementacao/Classe.Calculadora.pas
+++ Projeto 100 POO I - Interfaces Declaracao e Implementacao/Classe.Calculadora.pas
@@ -3,36 +3,9 @@ unit Classe.Calculadora;
 interface
 
 uses
-  Vcl.Controls;
+  Vcl.Controls, Calculadora.Interfaces;
 
 type
-  iOperacoes = interface
-    ['{5F27364B-FAEC-4838-8CFE-BC204D119EA0}']
-    function Operacao(Num1, Num2: double): double; overload;
-    function Operacao(Num1, Num2: String): String; overload;
-  end;
-
-
-  iCalculadora = interface
-   ['{E7A1CC56-45A6-43C5-B753-85883C2E5D75}']
-   function Soma: iOperacoes;
-   function Subtrair: iOperacoes;
-   function Dividir: iOperacoes;
-   function Multiplicar: iOperacoes;
-  end;
-
-
-
-  TCaptionHelper = record helper for TCaption
-    function ToCurrency: Currency;
-  end;
-
-  TStrHelper = record helper for String
-    function ToCurrency: Currency;
-  end;
-
-
-
   TCalculadora = class(TInterfacedObject, iCalculadora)
   private
     function Soma: iOperacoes;
@@ -47,166 +20,12 @@ type
   end;
 
 
-  TSoma = class(TInterfacedObject, iOperacoes)
-    function Operacao(Num1, Num2: double): double; overload;
-    function Operacao(Num1, Num2: String): String; overload;
-  public
-    constructor Create();
-    destructor Destroy(); override;
-
-    class function New(): iOperacoes;
-  end;
-
-  TSubtrair = class(TInterfacedObject, iOperacoes)
-    function Operacao(Num1, Num2: double): double; overload;
-    function Operacao(Num1, Num2: String): String; overload;
-  public
-    constructor Create();
-    destructor Destroy(); override;
-
-    class function New(): iOperacoes;
-  end;
-
-  TMultiplicar = class(TInterfacedObject, iOperacoes)
-    function Operacao(Num1, Num2: double): double; overload;
-    function Operacao(Num1, Num2: String): String; overload;
-  public
-    constructor Create();
-    destructor Destroy(); override;
-
-    class function New(): iOperacoes;
-  end;
-
-  TDividir = class(TInterfacedObject, iOperacoes)
-    function Operacao(Num1, Num2: double): double; overload;
-    function Operacao(Num1, Num2: String): String; overload;
-  public
-    constructor Create();
-    destructor Destroy(); override;
-
-    class function New(): iOperacoes;
-  end;
-
 implementation
 
 uses
-  System.SysUtils;
+  System.SysUtils, Calculadora.Dividir, Calculadora.Multiplicar,
+  Calculadora.Soma, Calculadora.Subtrair;
 
-{ TSoma }
-
-constructor TSoma.Create;
-begin
-
-end;
-
-destructor TSoma.Destroy;
-begin
-
-  inherited;
-end;
-
-class function TSoma.New: iOperacoes;
-begin
-  Result := Self.Create();
-end;
-
-function TSoma.Operacao(Num1, Num2: String): String;
-begin
-  Result := (Num1.ToCurrency + Num2.ToCurrency).ToString;
-end;
-
-function TSoma.Operacao(Num1, Num2: double): double;
-begin
-  Result := Num1 + Num2;
-end;
-
-
-{ TSubtrair }
-
-constructor TSubtrair.Create;
-begin
-
-end;
-
-destructor TSubtrair.Destroy;
-begin
-
-  inherited;
-end;
-
-class function TSubtrair.New: iOperacoes;
-begin
-  Result := Self.Create();
-end;
-
-function TSubtrair.Operacao(Num1, Num2: String): String;
-begin
-  Result := (Num1.ToCurrency - Num2.ToCurrency).ToString;
-end;
-
-function TSubtrair.Operacao(Num1, Num2: double): double;
-begin
-  Result := Num1 - Num2;
-end;
-
-{ TMultiplicar }
-
-constructor TMultiplicar.Create;
-begin
-
-end;
-
-destructor TMultiplicar.Destroy;
-begin
-
-  inherited;
-end;
-
-class function TMultiplicar.New: iOperacoes;
-begin
-  Result := Self.Create();
-end;
-
-function TMultiplicar.Operacao(Num1, Num2: String): String;
-begin
-  Result := (Num1.ToCurrency * Num2.ToCurrency).ToString;
-end;
-
-function TMultiplicar.Operacao(Num1, Num2: double): double;
-begin
-  Result := Num1 * Num2;
-end;
-
-{ TDividir }
-
-constructor TDividir.Create;
-begin
-
-end;
-
-destructor TDividir.Destroy;
-begin
-
-  inherited;
-end;
-
-class function TDividir.New: iOperacoes;
-begin
-  Result := Self.Create();
-end;
-
-function TDividir.Operacao(Num1, Num2: String): String;
-begin
-  Result := (Num1.ToCurrency / Num2.ToCurrency).ToString;
-end;
-
-function TDividir.Operacao(Num1, Num2: double): double;
-begin
-  if Num2 <= 0 then
-    raise Exception.Create('Valor inválido');
-
-  Result := Num1 / Num2;
-end;
 
 { TCalculadora }
 
@@ -244,20 +63,6 @@ end;
 function TCalculadora.Subtrair: iOperacoes;
 begin
   Result := TSubtrair.New();
-end;
-
-{ TCaptionHelper }
-
-function TCaptionHelper.ToCurrency: Currency;
-begin
-  Result := StrToCurr(Self);
-end;
-
-{ TStrHelper }
-
-function TStrHelper.ToCurrency: Currency;
-begin
-  Result := StrToCurr(Self);
 end;
 
 end.
